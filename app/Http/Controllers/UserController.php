51d0597e3343374117ad13793eb147a1d7d88f79
Elijah <elidoom88@gmail.com>
2020-07-06 10:49:08 +0200

    User profile roles

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,49 +4,70 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\User;
+use App\Role;
 
 class UserController extends Controller
 {
     //
 
-    public function index(){
+    public function index()
+    {
 
-        $users= User::all();
+        $users = User::all();
 
-        return view('admin.users.index',['users' => $users]);
+        return view('admin.users.index', ['users' => $users]);
     }
 
 
-    public function show(User $user){
+    public function show(User $user)
+    {
 
-        return view('admin.users.profile',['user'=>$user]);
+        return view(
+            'admin.users.profile',
+            [
+                'user' => $user,
+                'roles' => Role::all()
+            ]
+        );
     }
+ 
 
-    public function update(User $user){
+     public function attach(User $user){
+     $user->roles()->attach(request('role'));
+     return back();
 
-       $inputs = request()->validate([
+     }
 
-       'username'=>['required','string','max:255','alpha_dash'],
-       'name'=>['required','string','max:255'],
-       'email'=>['required','email','max:255'],
-       'avatar'=>['file'],
-       
-       ]); 
-       
-        if(request('avatar')){
-         $inputs['avatar'] = request('avatar')->store('images');
+     public function detach(User $user){
+        $user->roles()->detach(request('role'));
+        return back();
+   
+        }
+   
+
+
+    public function update(User $user)
+    {
+
+        $inputs = request()->validate([
+
+            'username' => ['required', 'string', 'max:255', 'alpha_dash'],
+            'name' => ['required', 'string', 'max:255'],
+            'email' => ['required', 'email', 'max:255'],
+            'avatar' => ['file'],
+
+        ]);
+
+        if (request('avatar')) {
+            $inputs['avatar'] = request('avatar')->store('images');
         }
         $user->update($inputs);
         return back();
-
     }
-     public function destroy(User $user){
-         $user->delete();
-         session()->flash('user-deleted', 'This User has been deleted');
-         return back();
-
-     }
-    
-
-
+    public function destroy(User $user)
+    {
+        $user->delete();
+        session()->flash('user-deleted', 'This User has been deleted');
+        return back();
+    }
 }
--- routes/web/users.php
+++ routes/web/users.php
@@ -9,8 +9,12 @@ Route::middleware('auth')->group(function () {
    
     Route::put('/users/{user}/update', 'UserController@update')->name('user.profile.update');
 });
+
+
 Route::middleware('role:admin')->group(function () {
     Route::get('/users', 'UserController@index')->name('users.index');
+    Route::put('/users/{user}/attach', 'UserController@attach')->name('user.role.attach');
+    Route::put('/users/{user}/detach', 'UserController@detach')->name('user.role.detach');
 });
 
 Route::middleware(['can:view,user'])->group(function () {
